33af8dedb0a89a9fdafc84dacfae4080a9957391
Jan Holub <Jan-Holub@gmx.net>
2017-07-03 17:06:21 +0200

    Added a prefab for the crosshair and a script that makes the crosshair follow the mouse

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+	Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		this.camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// transform the mouse position to world coordinates
+		Vector3 mousePosition = camera.ScreenToWorldPoint (Input.mousePosition);
+		// set the crosshair to the mouse position
+		// z: -3.0 to set it in front of everything
+		this.transform.position = new Vector3 (mousePosition.x, mousePosition.y, -3.0f);
+	}
+}
