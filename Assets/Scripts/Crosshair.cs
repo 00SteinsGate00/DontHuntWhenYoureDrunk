faa8f5d62f6a4b62453b1bd2f5cc4f694893fac0
Jan Holub <Jan-Holub@gmx.net>
2017-07-04 17:35:42 +0200

    Added Gunshot and Reload sounds that are played when fireing

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -8,14 +8,22 @@ public class Crosshair : MonoBehaviour {
 
 	public Ammunition ammunition;
 
+	private AudioSource audio;
+
+	public AudioClip gunshot;
+	public AudioClip reload;
+	public AudioClip outOfAmmo;
+
 	// Use this for initialization
 	void Start () {
 		this.camera = Camera.main;
 
-		// Reference to the Ammunition UI elements Script
-//		this.ammunition = this.GetComponent<Ammunition> ();
+		// Audio Source
+		this.audio = this.GetComponent<AudioSource> ();
+
+
 	}
-	
+			
 	// Update is called once per frame
 	void Update () {
 		// transform the mouse position to world coordinates
@@ -25,10 +33,21 @@ public class Crosshair : MonoBehaviour {
 		this.transform.position = new Vector3 (mousePosition.x, mousePosition.y, -3.0f);
 
 		if (Input.GetMouseButtonDown (0)) {
-			this.ammunition.reduceBulletCount ();
+			if (this.ammunition.getBulletCount () > 0) {
+				this.ammunition.reduceBulletCount ();
+				// gunshot
+				this.audio.PlayOneShot (this.gunshot, 1);
+			} else {
+				// out of ammo
+				this.audio.PlayOneShot (this.outOfAmmo, 1);
+			}
+
+
 		}
 		else if (Input.GetMouseButtonDown(1)) {
 			this.ammunition.reload ();
+			// reload
+			this.audio.PlayOneShot (this.reload, 1);
 		}
 	}
 }
