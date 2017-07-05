0efdc0e7422b585514c575ed572973e9d570b6e2
Jan Holub <Jan-Holub@gmx.net>
2017-07-05 23:58:18 +0200

    Changed cloud occurance interval

--- Assets/Scripts/CloudManager.cs
+++ Assets/Scripts/CloudManager.cs
@@ -14,8 +14,8 @@ public class CloudManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.cloudTimer += Random.Range (0, 3);
-		if (this.cloudTimer % 200 == 0) {
+		this.cloudTimer += Random.Range (1, 10);
+		if (this.cloudTimer % 150 == 0) {
 			Vector3 sceneBounds = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height, 0));
 			Vector3 cloudPosition = new Vector3 ((sceneBounds.x + sceneBounds.x / 5) * -1, Random.Range (-sceneBounds.y+sceneBounds.y/3, sceneBounds.y-sceneBounds.y/10), -2f);
 			Instantiate (cloudPrefab, cloudPosition, Quaternion.identity);
