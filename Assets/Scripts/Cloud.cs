a245627bdf9df2a5cae703741999f124fa0af570
Jan Holub <Jan-Holub@gmx.net>
2017-07-05 23:54:24 +0200

    Added clouds that block the view

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float velocity = 2;
+		Vector3 move = new Vector3 (1.0f, 0.0f, 0.0f);
+
+		this.transform.position += move * velocity * Time.deltaTime;
+
+		// check if the bird is outside of the screen and delete it
+		Vector3 sceneBounds = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height, 0));
+		if (this.transform.position.x > sceneBounds.x + sceneBounds.x / 2) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/CloudManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour {
+
+	private int cloudTimer = 1;
+
+	public Cloud cloudPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.cloudTimer += Random.Range (0, 3);
+		if (this.cloudTimer % 200 == 0) {
+			Vector3 sceneBounds = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height, 0));
+			Vector3 cloudPosition = new Vector3 ((sceneBounds.x + sceneBounds.x / 5) * -1, Random.Range (-sceneBounds.y+sceneBounds.y/3, sceneBounds.y-sceneBounds.y/10), -2f);
+			Instantiate (cloudPrefab, cloudPosition, Quaternion.identity);
+		}
+	}
+}
